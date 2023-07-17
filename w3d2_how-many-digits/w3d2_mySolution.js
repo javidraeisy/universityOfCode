9adca6d4b195089bd7046f24ea810dc261453492
javidraeisy <javid.raeisy@gmail.com>
2023-07-18 00:01:51 +0100

    answered w3d2 question with notes

---
+++ w3d2_how-many-digits/w3d2_mySolution.js
@@ -0,0 +1,14 @@
+
+function digits(x) {
+  return Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
+}
+/********Code Ends*****************/
+//   Reference:
+//   https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript
+
+/* Given Helpful Links
+ Math.abs()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+ Math.sign()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+*/
