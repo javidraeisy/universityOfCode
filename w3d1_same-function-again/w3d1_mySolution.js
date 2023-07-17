06150cc8b75ec6594d533445bfd4f20d3024978b
javidraeisy <javid.raeisy@gmail.com>
2023-07-17 23:28:55 +0100

    answered question for w3d1

---
+++ w3d1_same-function-again/w3d1_mySolution.js
@@ -0,0 +1,19 @@
+function same(str) {
+  return function () {
+    return str;
+  };
+}
+
+//   Reference:
+//   https://javascript.info/currying-partials
+
+/* Given Helpful Links
+ Closures
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+ Understanding Closures in JavaScript
+   - https://blog.bitsrc.io/a-beginners-guide-to-closures-in-javascript-97d372284dda
+ JavaScript Function Closures
+   - https://www.w3schools.com/js/js_function_closures.asp
+ Closure
+   - https://www.tutorialsteacher.com/javascript/closure-in-javascript
+*/
