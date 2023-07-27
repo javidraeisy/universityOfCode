db49dc4d7d686097609b23ec44eb185ef38d005c
javidraeisy <javid.raeisy@gmail.com>
2023-07-27 20:15:20 +0100

    solved question w5d1_my-house

---
+++ w5d1_my-house/w5d1_mySolution.js
@@ -0,0 +1,18 @@
+function myHouse(num) {
+  return 6 + (num - 1) * 5;
+}
+
+/* Given Helpful Links
+ Matchstick Patterns
+   - https://www.transum.org/Maths/Activity/Matchstick_Patterns/
+ Conditional Operator
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+ JavaScript Math Object
+   - https://www.w3schools.com/Js/js_math.asp
+ For
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+ Loops and Iteration
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+ Arithmetic Progression
+   - https://byjus.com/maths/arithmetic-progression/
+*/
