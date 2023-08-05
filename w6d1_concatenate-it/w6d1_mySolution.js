bc7c71dfce537f29312d7e353bf0a684af1f0907
javidraeisy <javid.raeisy@gmail.com>
2023-08-05 17:00:43 +0100

    soled w6d1 question

---
+++ w6d1_concatenate-it/w6d1_mySolution.js
@@ -0,0 +1,16 @@
+function concatenate(...arr) {
+  return [].concat(...arr);
+}
+
+/* Given Helpful Links
+ Array.prototype.concat()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+ Array.prototype.flat()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+ Flatten Array using Array.flat()
+   - https://medium.com/dailyjs/flatten-array-using-array-flat-in-javascript-ee4d0b2423e5
+ Spread Syntax
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ arguments Object
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+*/
