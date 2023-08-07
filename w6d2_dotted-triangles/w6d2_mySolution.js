cdfcf2f89f83d6c11f9461e924983d8beafff73a
javidraeisy <javid.raeisy@gmail.com>
2023-08-07 13:24:00 +0100

    solved w6d2

---
+++ w6d2_dotted-triangles/w6d2_mySolution.js
@@ -0,0 +1,16 @@
+function triangle(num) {
+  return (num * (num + 1)) / 2;
+}
+
+/* Given Helpful Links
+ Triangular Number Sequence
+   - https://www.mathsisfun.com/algebra/triangular-numbers.html
+ Triangular Number 1
+   - https://en.wikipedia.org/wiki/Triangular_number
+ Triangular Number 2
+   - https://math.fandom.com/wiki/Triangular_number
+ Learn and Understand Recursion in JavaScript
+   - https://codeburst.io/learn-and-understand-recursion-in-javascript-b588218e87ea
+ For Loop
+   - https://www.w3schools.com/js/js_loop_for.asp
+*/
